981881d0e3619be203ccfb15ac0383c73c10c68b
Rafa <108197293+Rafacampos16@users.noreply.github.com>
2024-03-24 00:26:20 -0300

    Add files via upload

---
+++ EX08/app.js
@@ -0,0 +1,12 @@
+console.log("Feliz Ano novo!");
+console.log("Feliz Ano novo!");
+console.log("Feliz Ano novo!");
+
+alert("Feliz Ano novo");
+alert("Feliz Ano novo");
+alert("Feliz Ano novo");
+
+alert("Feliz Ano novo \n Feliz Ano novo \n Feliz Ano novo");
+
+document.getElementById('resultado').innerHTML = `<p>Feliz Ano novo!</p> <p>Feliz Ano novo!</p
+<p>Feliz Ano novo!</p> `
---
+++ EX09/app.js
@@ -0,0 +1,6 @@
+console.log(8*8*8);
+
+alert(8*8*8);
+
+document.getElementById('resultado').innerHTML = 8*8*8; 
+
